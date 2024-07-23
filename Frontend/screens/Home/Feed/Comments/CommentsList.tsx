8eb54af79349ec30435d9cf43a48d5390b7a7e20
Jz <cjianzhi23@gmail.com>
2024-07-23 20:49:18 +0800

    Removed old comment.tsx file

--- Frontend/screens/Home/Feed/Comments/CommentsList.tsx
+++ Frontend/screens/Home/Feed/Comments/CommentsList.tsx
@@ -11,7 +11,6 @@ import {
   FlatList,
 } from "react-native";
 import React, { useState } from "react";
-// import Comment from "./Comment";
 import Comment from "@/components/social/comments/Comment";
 
 export default function CommentsList({ comments }: any) {
