1bd0febcc96bb0b3f3522c2e694cb83be8ae3bc2
ngjianyi <ngjianyi2002@gmail.com>
2024-08-06 17:49:22 +0800

    removed login page comments

--- Frontend/screens/Authentication/LoginScreen.tsx
+++ Frontend/screens/Authentication/LoginScreen.tsx
@@ -57,15 +57,6 @@ export default function LoginScreen({ navigation }: PropsLogin) {
       }
     } catch (error: any) {
       const errorCode = error.code;
-      // if (errorCode == "auth/invalid-email") {
-      //   alert("Invalid email provided");
-      // } else if (errorCode == "auth/invalid-credential") {
-      //   alert("Invalid credentials provided");
-      // } else if (errorCode == "auth/missing-password") {
-      //   alert("Please enter password");
-      // } else {
-      //   alert("Login failed, invalid credentials");
-      // }
       if (
         errorCode == "auth/invalid-email" ||
         errorCode == "auth/invalid-password" ||
@@ -95,14 +86,12 @@ export default function LoginScreen({ navigation }: PropsLogin) {
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
           <Image source={logoImg} style={styles.logo} />
-          {/* <View style={styles.headerText}>
-            <Text style={styles.subHeaderTextHello}>Hello! </Text>
-            <Text style={styles.subHeaderText}>Welcome back to DumoEats!</Text>
-          </View> */}
+
           <View style={styles.welcomeContainer}>
             <Text style={styles.welcomeText}>Welcome back!</Text>
           </View>
         </View>
+
         <View style={styles.details}>
           <TextInput
             style={styles.input}
@@ -132,6 +121,7 @@ export default function LoginScreen({ navigation }: PropsLogin) {
               autoComplete="off"
               aria-label="Password"
             />
+
             <TouchableOpacity style={styles.visible} onPress={pressHandler}>
               {visible ? (
                 <Ionicons name="eye" size={22} color={COLORS.gray} />
@@ -140,54 +130,29 @@ export default function LoginScreen({ navigation }: PropsLogin) {
               )}
             </TouchableOpacity>
           </View>
-          {/* <TouchableOpacity onPress={forgetHandler}>
-            <Text style={{ textAlign: "right", color: "dodgerblue" }}>
-              Forgot password?
-            </Text>
-          </TouchableOpacity> */}
+
           <View style={styles.forgetPasswordContainer}>
             <TouchableOpacity onPress={forgetHandler}>
               <Text style={styles.blueText}>Forgot password?</Text>
             </TouchableOpacity>
           </View>
         </View>
-        {/* <TouchableOpacity
-          style={styles.loginButton}
-          onPress={handleSubmit}
-          aria-label="loginButton"
-        >
-          <Text style={styles.login}>Sign In</Text>
-        </TouchableOpacity> */}
+
         {!loading ? (
           <TouchableOpacity
             style={styles.loginButton}
             onPress={handleSubmit}
             aria-label="loginButton"
           >
-            <Text style={styles.login}>Sign In</Text>
+            <Text style={styles.login}>Log In</Text>
           </TouchableOpacity>
         ) : (
           <ActivityIndicator size="large" />
         )}
-        {/* {loading ? (
-          <ActivityIndicator size="large" color="deepskyblue" />
-        ) : (
-          true
-        )} */}
+
         <View style={styles.noAccountContainer}>
-          {/* <View>
-          <Text style={{ textAlign: "center" }}>
-            Don't have an account yet?
-          </Text>
-          <TouchableOpacity
-            style={styles.signupButton}
-            onPress={signupHandler}
-            aria-label="signupButton"
-          >
-            <Text style={styles.signUp}>Sign Up</Text>
-          </TouchableOpacity>
-          </View> */}
           <Text style={styles.defaultText}>Don't have an account?</Text>
+
           <TouchableOpacity onPress={signupHandler} aria-label="signupButton">
             <Text style={styles.signupText}>Sign up</Text>
           </TouchableOpacity>
@@ -200,7 +165,6 @@ export default function LoginScreen({ navigation }: PropsLogin) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "white",
     backgroundColor: COLORS.lightWhite,
   },
 
@@ -209,33 +173,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 
-  // headerText: {
-  //   flexDirection: "row",
-  // },
-
-  // subHeaderTextHello: {
-  //   fontWeight: "bold",
-  //   marginVertical: 10,
-  //   color: "darkgreen",
-  // },
-
-  // subHeaderText: {
-  //   fontWeight: "bold",
-  //   marginVertical: 10,
-  // },
-
   details: {
-    // margin: 5,
-    // padding: 35,
     margin: SIZES.xxLarge,
   },
 
   input: {
-    // padding: 10,
-    // marginVertical: 10,
-    // backgroundColor: "lavender",
-    // borderRadius: 5,
-    // backgroundColor: COLORS.gray2,
     borderWidth: SIZES.xSmall / 8,
     borderColor: COLORS.gray,
     marginVertical: SIZES.small,
@@ -245,27 +187,20 @@ const styles = StyleSheet.create({
 
   visible: {
     position: "absolute",
-    // right: 12,
     right: SIZES.xSmall,
   },
 
   loginButton: {
-    // backgroundColor: "springgreen",
     backgroundColor: COLORS.tertiary,
-    // marginTop: 40,
-    // marginBottom: 30,
-    // marginHorizontal: 30,
     marginHorizontal: SIZES.xxLarge,
     borderRadius: SIZES.xSmall * 2,
-    // marginHorizontal: SIZES.xxLarge * 2,
-    // padding: 5,
     padding: SIZES.xSmall / 2,
   },
 
   login: {
     textAlign: "center",
-    fontSize: 18,
-    padding: 5,
+    fontSize: SIZES.large,
+    padding: SIZES.xSmall / 2,
     color: "black",
   },
 
@@ -301,10 +236,6 @@ const styles = StyleSheet.create({
     marginVertical: SIZES.xSmall / 4,
   },
   blueText: {
-    // textAlign: "center",
-    // fontSize: 18,
-    // padding: 5,
-    // color: "white",
     fontSize: SIZES.medium,
     color: COLORS.blue,
     marginVertical: SIZES.xSmall / 4,
